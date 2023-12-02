dba550061e4f08ad441cf1ae49cb1e707445bf60
Marc-Aurèle Brothier <m@brothier.org>
2023-12-02 08:46:05 +0100

    day 01

---
+++ day01/trebuchet.py
@@ -0,0 +1,74 @@
+import re
+import click
+
+SWITCH = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+    "0": 0,
+    "1": 1,
+    "2": 2,
+    "3": 3,
+    "4": 4,
+    "5": 5,
+    "6": 6,
+    "7": 7,
+    "8": 8,
+    "9": 9,
+}
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument("input", type=click.File("r"))
+def part1(input):
+    p = re.compile("(\\d)")
+    sum = 0
+    for line in input.readlines():
+        try:
+            # click.echo(line[:-1], nl=False)
+            m = p.findall(line)
+            if len(m) == 0:
+                continue
+            else:
+                sum += int(f"{m[0]}{m[-1]}")
+                # val = int(f"{m[0]}{m[-1]}")
+            # click.echo(f" -> {m} -> {val} -> {sum}")
+        except ValueError:
+            click.echo("Invalid number", err=True, fg="red")
+    click.echo(f"Calibration value: {sum}")
+
+
+@cli.command()
+@click.argument("input", type=click.File("r"))
+def part2(input):
+    p = re.compile("(\\d|one|two|three|four|five|six|seven|eight|nine)")
+    sum = 0
+    for line in input.readlines():
+        try:
+            # click.echo(line[:-1], nl=False)
+            m1 = p.search(line)
+            for i in range(1, len(line)+1):
+                m2 = p.search(line[i * -1:])
+                if m2:
+                    break
+
+            sum += SWITCH.get(m1.group(0)) * 10 + SWITCH.get(m2.group(0))
+            # click.echo(f" -> {m1.group(0)}{m2.group(0)} -> {sum}")
+        except ValueError:
+            click.echo("Invalid number", err=True, fg="red")
+    click.echo(f"Calibration value: {sum}")
+
+
+if __name__ == "__main__":
+    cli()
